fc65be0c167566031f3aed43b2742f967adea043
Hugo-Azix <hugo.millet@isen.yncrea.fr>
2023-04-23 04:05:32 -0500

    enelever fichier otp.js

--- src/js/otp.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("generateOTP").addEventListener("click", function() {
-  // Appeler l'API pour générer l'OTP
-  fetch("/generate_otp")
-  .then(response => response.json())
-});
